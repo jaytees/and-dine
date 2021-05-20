27075c4652db1b0396a6694881f76760a2c7505c
sean milburn <sean.milburn@just-eat.com>
2021-05-20 15:22:47 +0100

    Added new logic to middleware

--- api/mailchimp.js
+++ api/mailchimp.js
@@ -1,25 +1,26 @@
 import express from 'express'
 import Mailchimp from 'mailchimp-api-v3'
 
-const API_KEY = process.env.MAILCHIMP_API_KEY || '<YOUR_AUDIENCE_ID>'
-const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID || '<YOUR_API_KEY>'
+const API_KEY = '02d02a91ab778f5992225144904ec65f-us1'
+const AUDIENCE_ID = '5c8512abdc'
 const mailchimp = new Mailchimp(API_KEY)
 
 const app = express()
 app.use(express.json())
 
 app.post('/subscribe', async (req, res) => {
-  const { email: email_address } = req.body
+  const { email } = req.body
 
   try {
     const response = await mailchimp.request({
       method: 'post',
       path: `/lists/${AUDIENCE_ID}/members`,
       body: {
-        email_address,
+        email_address: email,
         status: 'subscribed',
       },
     })
+    console.log(response)
     res.status(response.statusCode).json(response.status)
   } catch (error) {
     res.status(error.status).send(error)
