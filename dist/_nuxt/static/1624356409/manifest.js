2c242f52309ca6d66c6833317020b3fbd033c00e
sean milburn <sean.milburn@just-eat.com>
2021-06-27 11:13:34 +0100

    Rebuilt

--- dist/_nuxt/static/1624356409/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fsellers\u002Fseanies-sausages","\u002Fsubscribe","\u002Fsellers\u002Fcaroline-wilde","\u002Fsellers\u002Fcoming-soon","\u002Fsellers\u002F-dine-test","\u002Fjoin","\u002Fabout","\u002Fsellers\u002Fmicky-drippings","\u002Fsellers\u002Fcoming-soon-2","\u002Fsellers","\u002F"]})
